6a261e0f815e280950c565366fda462f3c411523
ew8 <ew8_github@users.noreply.github.com>
2021-03-01 19:00:05 +0800

    init basic

---
+++ src/main/java/com/example/plugin/test.java
@@ -0,0 +1,16 @@
+package com.example.plugin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class test {
+	public static void main(String[] args) {
+		List<String> a=new ArrayList<>();
+		a.add("1");
+		a.add("1");
+		a.add("1");
+		a.add("1");
+		a.add("1");
+		System.out.println(a.toString());
+	}
+}
